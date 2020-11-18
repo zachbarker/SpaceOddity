be0b19b5cf124332ff3ea889cf9e72663a6c219a
JasSC97 <Montreal231>
2020-11-18 14:31:30 -0800

    need ot still debug but have code for connection client-server via websocket and then making UDP datachannel from that conneciton and finally closing initial websocket.

--- back_end/server.go
+++ back_end/server.go
@@ -20,28 +20,91 @@ func makeWebSocket(w http.ResponseWriter, r *http.Request) {
 
 	defer playerSocket.Close()
 
-	offer := []byte("base64encodedoffer")
+	config := webrtc.Configuration{
+		ICEServers: []webrtc.ICEServer{
+			{
+				URLs: []string{"stun:stun.l.google.com:19302"}	
+			},
+		},
+	}
 
-	err = playerSocket.WriteMessage(websocket.TextMessage, offer)
+	// now that our web socket connection is successful, we
+	// start prepping our peer connection and offer.
 
+	peerConnection, err := webrtc.NewPeerConnection(config)
+	if err != nil {
+		log.Println("Could not establish peer connection: ", err)
+		return
+	}
+
+	dcConfig := webrtc.DataChannelInit{}
+	ordered := false
+	transmits := uint16(0)
+	dcConfig.Ordered = &ordered
+	dcConfig.MaxRetransmits = &transmits
+
+	dataChannel, err := peerConnection.CreateDataChannel("player-connection", &dcConfig)
+	if err != nil {
+		log.Println("Could not successfuly create a data channel: ", err)
+		return
+	}
+
+	dataChannel.OnOpen(func() {
+		fmt.Printf("Data channel '%s'-'%d' open.\n", dataChannel.Label(), dataChannel.ID())
+
+		for range time.NewTicker(5 * time.Second).C {
+			message := "message"
+			fmt.Printf("Sending '%s'\n", message)
+
+			// Send the message as text
+			sendErr := dataChannel.SendText(message)
+			if sendErr != nil {
+				log.Println(sendErr)
+				return
+			}
+		}	
+	})
+
+	offer, err := peerConnection.CreateOffer(nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	gatherComplete := peerConnection.GatheringCompletePromise(peerConnection)
+
+	err = peerConnection.SetLocalDescription(offer)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	<-gatherComplete
+
+	encodedOffer = Encode(*peerConnection.LocalDescription())
+	err = playerSocket.WriteMessage(websocket.TextMessage, encodedOffer)
 	if err != nil {
 		log.Print("ERROR", err)
 		return
 	}
 
-	_, answer, err := playerSocket.ReadMessage()
-
-	fmt.Printf("received answer! %s\n", answer)
-
+	_, encodedAnswer, err := playerSocket.ReadMessage()
 	if err != nil {
 		log.Print("error when trying to grab answer from client", err)
 		return
 	}
 
+	fmt.Printf("received answer! %s\n", answer)
+	decodedAnswer := Decode(answer)
+
+	err = peerConnection.SetRemoteDescription(decodedAnswer)
+	if err != nil {
+		log.Printf("error when trying to create data channel")
+		return
+	}
 	// need to create a webrtc data channel here.
 
-	err = playerSocket.WriteMessage(websocket.TextMessage, []byte("DC MADE"))
-
+	err = playerSocket.WriteMessage(websocket.TextMessage, []byte("DC MADE")
 }
 
 func main() {
