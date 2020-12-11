ed5cc6228ac16136adbc12f5527aeab1765f499b
zachbarker <zach-barker@hotmail.com>
2020-12-11 00:07:19 -0800

    Added random spawning and background

--- front_end/js/asteroid_physics.js
+++ front_end/js/asteroid_physics.js
@@ -13,11 +13,16 @@ var config = {
     scene: {
         preload: preload,
         create: create,
+        update: update,
         extend: {
-            spawnAsteroids: spawnAsteroids
+            spawnAsteroids: spawnAsteroids,
+            randomSpawnSmall: randomSpawnSmall,
+            randomSpawnMedium: randomSpawnMedium,
+            randomSpawnLarge: randomSpawnLarge
         }
     }
 };
+
 
 // // var timer = scene.time.addEvent({
 //     delay: 500,                // ms
@@ -29,13 +34,38 @@ var config = {
 
 const game = new Phaser.Game(config)
 
+let current_tick = "none"
+var asteroids
+var num_asts = 89
+
+
 function create() {
+
+    this.add.image(400, 300, 'background');
+
     //asteroids 
     asteroids = this.physics.add.group()
 
+    // ticks = this.physics.add.group()
+
     //ship and bullets
     ship = this.physics.add.sprite(0, 0, 'sprites')
+    ship.displayWidth = 40;
+    ship.displayHeight = 40;
     bullets = this.physics.add.group()
+
+    //shooting physics
+    this.input.on('pointerdown', function (pointer) {
+        let angle = Phaser.Math.Angle.Between(ship.x, ship.y, pointer.x, pointer.y)
+        let h = Phaser.Math.Distance.Between(ship.x, ship.y, pointer.x, pointer.y)
+        fire(angle, h)
+    }, this)
+
+    ship.setCollideWorldBounds(true)
+    w = this.input.keyboard.addKey('W')
+    a = this.input.keyboard.addKey('A')
+    s = this.input.keyboard.addKey('S')
+    d = this.input.keyboard.addKey('D')
 
     this.anims.create({
         key: 'kaboom',
@@ -45,42 +75,39 @@ function create() {
         }),
         frameRate: 16,
         repeat: 0,
-        hideOnComplete: false
+        hideOnComplete: true
     })
     //explosions 
     explosions = this.add.group({
         defaultKey: 'explosion'
     })
-    
-    this.physics.add.collider(bullets, asteroids, hit)
-    // this.physics.arcade.collide(asteroids, bullets, hit)
-    
-    //shooting physics
-    this.input.on('pointerdown', function (pointer) {
-        let angle = Phaser.Math.Angle.Between(ship.x, ship.y, pointer.x, pointer.y)
-        let h = Phaser.Math.Distance.Between(ship.x, ship.y, pointer.x, pointer.y)
-        fire(angle, h)
-    }, this)
-    
-    ship.setCollideWorldBounds(true)
-    w = this.input.keyboard.addKey('W')
-    a = this.input.keyboard.addKey('A')
-    s = this.input.keyboard.addKey('S')
-    d = this.input.keyboard.addKey('D')
-    // adds an event every 1000ms to spawn a random asteroid.
-    this.time.addEvent({ delay: 1000, callback: spawnAsteroids, callbackScope: this, loop: true });
 
+    // ticks = this.add.text(16, 16, 'tickParameter', { fontSize: '32px', fill: '#000' });
+    ticks = this.physics.add.group()
+    tick = this.add.text(16, 16, 'tickParameter', { fontSize: '32px', fill: '#000' })
+
+    this.physics.add.collider(bullets, asteroids, shootAsteroid)
+    this.physics.add.collider(ship, asteroids, hitAsteroid)
+    // this.physics.arcade.collide(asteroids, bullets, hit)
+
+    // adds an event every 1000ms to spawn a random asteroid.
+    this.time.addEvent({ delay: 750, callback: spawnAsteroids, callbackScope: this, loop: true });
 }
 
-function hit(bullet, asteroid) {
-
+function shootAsteroid(bullet, asteroid) {
     let explosion = explosions.create(asteroid.x, asteroid.y, 'explosion')
-    explosion.on("animationcomplete", ()=> explosion.destroy())
+    explosion.on("animationcomplete", () => explosion.destroy())
     explosion.play('kaboom')
     bullet.destroy()
     asteroid.destroy()
-   
-    
+}
+
+function hitAsteroid(ship, asteroid) {
+    // asteroid.body.setBounce(0.1,0.1)
+    // let explosion = explosions.create(ship.x, ship.y, 'explosion')
+    // explosion.on("animationcomplete", () => explosion.destroy())
+    // explosion.play('kaboom')
+    // // ship.destroy()
 }
 
 function preload() {
@@ -96,37 +123,257 @@ function preload() {
         frameWidth: 16, frameHeight: 16
     })
     this.load.image('bullet', 'assets/images/bomb.png')
+    this.load.image('background', 'assets/images/space_background.png')
 }
-
 
 function update() {
     shipMovement()
-    
-    this.physics.arcarde.collide()
-
+    spawnspawn()
 }
 
+// function spawnAsteroids() {
+
+//     let ind = Phaser.Math.Between(0, 89)
+//     let asteroid = asteroids.create(800, 300, 'asteroids', ind);
+//     // asteroid.setBounce(1, 1)
+//     asteroid.displayWidth = 60;
+//     asteroid.displayHeight = 60;
+
+//     asteroid.setVelocity(-50, 0);
+//     asteroid.body.setAllowGravity(false)
+//     asteroid.setCollideWorldBounds(false)
+// }
+
+// randomly chooses one of the asteroid sizes and
+// spawns the selected asteroid on a random side of the map
 function spawnAsteroids() {
 
-    let ind = Phaser.Math.Between(0, 89)
-    let asteroid = asteroids.create(800, 300, 'asteroids', ind);
+    var temp = (Math.floor((Math.random() * 3) + 1));
 
-    asteroid.displayWidth = 60;
-    asteroid.displayHeight = 60;
-
-    asteroid.setVelocity(-50, 0);
-    asteroid.body.setAllowGravity(false);
-    asteroid.setCollideWorldBounds(false);
+    if (temp == 1) {
+        this.randomSpawnSmall();
+    } else if (temp == 2) {
+        this.randomSpawnMedium();
+    } else {
+        this.randomSpawnLarge();
+    }
 }
 
+// random number generator for randomizing direction of travel
+function randomNum(min, max) {
+    return Math.random() * (max - min) + min;
+}
+
+// Medium asteroids
+function randomSpawnMedium() {
+
+    // randomizing the side of the screen the medium asteroid spawns
+    // and assigns the direction they travel accordingly
+    if (Math.floor((Math.random() * 4) + 1) == 1) {
+
+        randomX = randomNum(0, 800);
+        randomY = randomNum(0, 600);
+
+        var ind = Phaser.Math.Between(0, num_asts)
+        var asteroid = asteroids.create(randomX, 600, 'asteroids', ind);
+
+        asteroid.displayWidth = 60;
+        asteroid.displayHeight = 60;
+
+        asteroid.setVelocity(randomNum(-100, 100), randomNum(-100, 0));
+        asteroid.body.setAllowGravity(false);
+        asteroid.setCollideWorldBounds(false);
+
+    } else if (Math.floor((Math.random() * 4) + 1) == 2) {
+
+        randomX = randomNum(0, 800);
+        randomY = randomNum(0, 600);
+
+        var ind = Phaser.Math.Between(0, num_asts)
+        var asteroid = asteroids.create(0, randomY, 'asteroids', ind);
+
+        asteroid.displayWidth = 60;
+        asteroid.displayHeight = 60;
+
+        asteroid.setVelocity(randomNum(0, 100), randomNum(-100, 100));
+        asteroid.body.setAllowGravity(false);
+        asteroid.setCollideWorldBounds(false);
+
+    } else if (Math.floor((Math.random() * 4) + 1) == 3) {
+
+        randomX = randomNum(0, 800);
+        randomY = randomNum(0, 600);
+
+        var ind = Phaser.Math.Between(0, num_asts)
+        var asteroid = asteroids.create(randomX, 0, 'asteroids', ind);
+
+        asteroid.displayWidth = 60;
+        asteroid.displayHeight = 60;
+
+        asteroid.setVelocity(randomNum(-100, 100), randomNum(0, 100));
+        asteroid.body.setAllowGravity(false);
+        asteroid.setCollideWorldBounds(false);
+
+    } else {
+        randomX = randomNum(0, 800);
+        randomY = randomNum(0, 600);
+
+        var ind = Phaser.Math.Between(0, num_asts)
+        var asteroid = asteroids.create(800, randomY, 'asteroids', ind);
+
+        asteroid.displayWidth = 60;
+        asteroid.displayHeight = 60;
+
+        asteroid.setVelocity(randomNum(-100, 0), randomNum(-100, 100));
+        asteroid.body.setAllowGravity(false);
+        asteroid.setCollideWorldBounds(false);
+    }
+
+}
+
+
+// Small asteroids
+function randomSpawnSmall() {
+
+    // randomizing the side of the screen the small asteroid spawns
+    // and assigns the direction they travel accordingly
+    if (Math.floor((Math.random() * 4) + 1) == 1) {
+
+        randomX = randomNum(0, 800);
+        randomY = randomNum(0, 600);
+
+        var ind = Phaser.Math.Between(0, num_asts)
+        var asteroid = asteroids.create(randomX, 600, 'asteroids', ind);
+
+        asteroid.displayWidth = 30;
+        asteroid.displayHeight = 30;
+
+        asteroid.setVelocity(randomNum(-100, 100), randomNum(-100, 0));
+        asteroid.body.setAllowGravity(false);
+        asteroid.setCollideWorldBounds(false);
+
+    } else if (Math.floor((Math.random() * 4) + 1) == 2) {
+
+        randomX = randomNum(0, 800);
+        randomY = randomNum(0, 600);
+
+        var ind = Phaser.Math.Between(0, num_asts)
+        var asteroid = asteroids.create(0, randomY, 'asteroids', ind);
+
+        asteroid.displayWidth = 30;
+        asteroid.displayHeight = 30;
+
+        asteroid.setVelocity(randomNum(0, 100), randomNum(-100, 100));
+        asteroid.body.setAllowGravity(false);
+        asteroid.setCollideWorldBounds(false);
+
+    } else if (Math.floor((Math.random() * 4) + 1) == 3) {
+
+        randomX = randomNum(0, 800);
+        randomY = randomNum(0, 600);
+
+        var ind = Phaser.Math.Between(0, num_asts)
+        var asteroid = asteroids.create(randomX, 0, 'asteroids', ind);
+
+        asteroid.displayWidth = 30;
+        asteroid.displayHeight = 30;
+
+        asteroid.setVelocity(randomNum(-100, 100), randomNum(0, 100));
+        asteroid.body.setAllowGravity(false);
+        asteroid.setCollideWorldBounds(false);
+
+    } else {
+        randomX = randomNum(0, 800);
+        randomY = randomNum(0, 600);
+
+        var ind = Phaser.Math.Between(0, num_asts)
+        var asteroid = asteroids.create(800, randomY, 'asteroids', ind);
+
+        asteroid.displayWidth = 30;
+        asteroid.displayHeight = 30;
+
+        asteroid.setVelocity(randomNum(-100, 0), randomNum(-100, 100));
+        asteroid.body.setAllowGravity(false);
+        asteroid.setCollideWorldBounds(false);
+    }
+}
+
+// Large Asteroids
+function randomSpawnLarge() {
+
+    // randomizing the side of the screen the large asteroid spawns
+    // and assigns the direction they travel accordingly
+    if (Math.floor((Math.random() * 4) + 1) == 1) {
+
+        randomX = randomNum(0, 800);
+        randomY = randomNum(0, 600);
+
+        var ind = Phaser.Math.Between(0, num_asts)
+        var asteroid = asteroids.create(randomX, 600, 'asteroids', ind);
+
+        asteroid.displayWidth = 90;
+        asteroid.displayHeight = 90;
+
+        asteroid.setVelocity(randomNum(-100, 100), randomNum(-100, 0));
+        asteroid.body.setAllowGravity(false);
+        asteroid.setCollideWorldBounds(false);
+
+    } else if (Math.floor((Math.random() * 4) + 1) == 2) {
+
+        randomX = randomNum(0, 800);
+        randomY = randomNum(0, 600);
+
+        var ind = Phaser.Math.Between(0, num_asts)
+        var asteroid = asteroids.create(0, randomY, 'asteroids', ind);
+
+        asteroid.displayWidth = 90;
+        asteroid.displayHeight = 90;
+
+        asteroid.setVelocity(randomNum(0, 100), randomNum(-100, 100));
+        asteroid.body.setAllowGravity(false);
+        asteroid.setCollideWorldBounds(false);
+
+    } else if (Math.floor((Math.random() * 4) + 1) == 3) {
+
+        randomX = randomNum(0, 800);
+        randomY = randomNum(0, 600);
+
+        var ind = Phaser.Math.Between(0, num_asts)
+        var asteroid = asteroids.create(randomX, 0, 'asteroids', ind);
+
+        asteroid.displayWidth = 90;
+        asteroid.displayHeight = 90;
+
+        asteroid.setVelocity(randomNum(-100, 100), randomNum(0, 100));
+        asteroid.body.setAllowGravity(false);
+        asteroid.setCollideWorldBounds(false);
+
+    } else {
+        randomX = randomNum(0, 800);
+        randomY = randomNum(0, 600);
+
+        var ind = Phaser.Math.Between(0, num_asts)
+        var asteroid = asteroids.create(800, randomY, 'asteroids', ind);
+
+        asteroid.displayWidth = 90;
+        asteroid.displayHeight = 90;
+
+        asteroid.setVelocity(randomNum(-100, 0), randomNum(-100, 100));
+        asteroid.body.setAllowGravity(false);
+        asteroid.setCollideWorldBounds(false);
+    }
+}
+
+
 function fire(angle, h) {
-    b = bullets.create(ship.x, ship.y, 'bullet');
+    b = bullets.create(ship.x, ship.y, 'bullet')
     b.setVelocityX(Math.cos(angle) * 400)
     b.setVelocityY(Math.sin(angle) * 400)
-
+    spawnspawn()
 }
 
 function shipMovement() {
+    console.log("moving")
 
     if (w.isDown) {
         if (a.isDown) {
@@ -190,5 +437,19 @@ function shipMovement() {
     }
 }
 
+let last_tick = 0
 
+function spawnspawn() {
+    let ct_lst = current_tick.split(" ")
+    let ct = ct_lst[2]
+    if (last_tick != ct) {
+        last_tick = ct
+
+        let new_tick = tick
+
+        new_tick.setText(current_tick)
+        ticks.add(new_tick)
+    } 
+
+}
 
